0fbdb6b89876943c6a320609f195e7b795060a6a
Aniket Chowdhury <aniket.chowdhury@rakuten.com>
2023-10-03 17:40:12 +0530

    added first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import RangeSlider from "./RangeSlider";
+import { useFetchAge } from "./hooks/fetchAge";
 
 function App() {
+  const headers = ["Id", "Name", "Gender", "Age", "Address", "Phone"];
+  const URL = `http://hapi.fhir.org/baseR4/Patient?_pretty=true`;
+  const [minVal, setMinVal] = useState(0);
+  const [maxVal, setMaxVal] = useState(0);
+  const actualData = useFetchAge(URL, minVal, maxVal);
+  const [filteredData, setFilteredData] = useState(actualData);
+  useEffect(() => {
+    let dataCopy = [];
+    actualData.forEach((item) => {
+      let temp = { ...item };
+      temp.age = item?.age === 0 ? "NA" : item.age;
+      if (item.age >= minVal && item.age <= maxVal) {
+        dataCopy.push(temp);
+      }
+    });
+    setFilteredData(dataCopy);
+  }, [actualData, maxVal, minVal]);
+  const getRange = (min, max) => {
+    setMinVal(min);
+    setMaxVal(max);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <RangeSlider getRange={getRange} />
+      <div className="mainContainer">
+        <h4>Scrollable Table</h4>
+        <table className="tableMain">
+          <th className="tableHeader">
+            {headers.map((item) => (
+              <td
+                style={{ border: "1px solid black", padding: "10px" }}
+                className="tableCells"
+                key={item}
+              >
+                {item}
+              </td>
+            ))}
+          </th>
+          <tbody style={{ border: "1px solid black" }}>
+            {filteredData.length > 0 &&
+              filteredData.map((item) => (
+                <tr
+                  style={{ border: "1px solid black" }}
+                  key={item.id}
+                  className="tableForm"
+                >
+                  {item &&
+                    Object.keys(item).map((x) => (
+                      <td className="tableCells">{item[x]}</td>
+                    ))}
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 }
 
